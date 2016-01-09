1b8cb13693b6158784af151b785ce92a8a0613f3
likeMyCode <patryk.gliszczynski94@gmail.com>
2016-01-09 18:53:52 +0100

    Create signin.h

---
+++ signin.h
@@ -0,0 +1,28 @@
+#ifndef SIGNIN_H
+#define SIGNIN_H
+
+#include <QWidget>
+
+namespace Ui {class SignIn;}
+
+class SignIn : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SignIn(QWidget *parent = 0);
+    ~SignIn();
+
+private slots:
+    void enterChat();
+    bool tryConnect();
+    void on_ipAddressEdit_returnPressed();
+    void on_portEdit_returnPressed();
+    void on_usernameEdit_returnPressed();
+    void on_startChatButton_clicked();
+
+private:
+    Ui::SignIn *ui;
+};
+
+#endif // SIGNIN_H
