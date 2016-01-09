03ab36b8f5a955f9a4bdb1b3ec3710b7fb0302d7
likeMyCode <patryk.gliszczynski94@gmail.com>
2016-01-09 19:58:42 +0100

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,278 +1,421 @@
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
 #include <string.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <iostream>
 #include <list>
 #include <time.h>
 #include <sstream>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 using namespace std;
+
+//-----------------------
+
+struct client {
+        int sck;
+        string name;
+        string color;
+        string chatRoom;
+};
+
+//-----------------------
 
 #define CON_LIMIT 128
 #define PORT 2000
 #define BUFSIZE 1024;
 
-struct client {
-	int sck;
-	string name;
-	string color;
-	string chatRoom;
-};
-
 pthread_t client_threads [CON_LIMIT];
 pthread_t main_thread;
 pthread_mutex_t sock_mutex = PTHREAD_MUTEX_INITIALIZER;
 int cln_sockets [CON_LIMIT];
-
 list<client> clients;
-string colorsList[] = { "DeepPink", "DodgerBlue", "DarkTurquoise", "ForestGreen", 
-			"Gold", "Indigo", "LightSalmon", "Crimson", "DarkOrange",
-			"DarkMagenta", "DarkViolet", "GoldenRod", "LightSeaGreen",
-			"MediumBlue", "Navy", "OrangeRed", "Orchid", "SeaGreen", 
-			"Sienna", "SaddleBrown", "SteelBLue", "Teal", "Tomato" };
 
-string chatRooms[] = { "Main Chat", "Book Lovers", "Music Fun", "Game Talk", 
-		       "Random Stuff", "YMCA" };
+string colorsList[] = { "DeepPink", "DodgerBlue", "DarkTurquoise", "ForestGreen",
+                        "Gold", "Indigo", "LightSalmon", "Crimson", "DarkOrange",
+                        "DarkMagenta", "DarkViolet", "GoldenRod", "LightSeaGreen",
+                        "MediumBlue", "Navy", "OrangeRed", "Orchid", "SeaGreen",
+                        "Sienna", "SaddleBrown", "SteelBLue", "Teal", "Tomato" };
 
+string chatRooms[] = { "Main Chat", "Book Lovers", "Music Fun", "Game Talk",
+                       "Random Stuff", "YMCA" };
 
+//-----------------------
+
+/** Funkcja zamieniajca integer na string
+ *
+ * @param integer Wartość do zamiany na string
+ * @return Zamieniony int na string
+ *
+ */
 string intToStr(int integer) {
-	stringstream intSS;
-	intSS << integer;
-	return intSS.str();
+
+        stringstream intSS;
+        intSS << integer;
+        return intSS.str();
 }
 
+
+
+/** Funkcja zwracająca losowy kolor z tablicy kolorów
+ *
+ * @return Losowy kolor
+ *
+ */
 string randomColor() {
-	int numberOfColors = sizeof(colorsList) / sizeof(colorsList[0]);
-	int randomNumber = rand() % numberOfColors;
 
-	return colorsList[randomNumber];
+        // Rozmiar tablicy kolorów
+        int numberOfColors = sizeof(colorsList) / sizeof(colorsList[0]);
+
+        // Losowy indeks tablicy colorsList
+        int randomNumber = rand() % numberOfColors;
+
+        return colorsList[randomNumber];
 }
 
 
+
+/** Funkcja odpowiedzialna za wysyłanie wiadomości do klienta
+ *
+ * @param sck Gniazdo do komunikacji z odpowiednim klientem
+ * @param messageType Rodzaj przesyłanej wiadomości
+ * @param message Przesyłana wiadomość
+ *
+ */
 void sendMessageToClient(int sck, string messageType, string message) {
-	
-	string finalMessage = messageType + ";" + intToStr(message.length()) + message;
-	cout << finalMessage.c_str() << endl;
-	send (sck, finalMessage.c_str(), finalMessage.length(), 0);
+
+    // Przygotowanie wartości końcowej złożonej z typu wiadomości; rozmiaru wiadomości; wiadomości
+    string finalMessage = messageType + ";" + intToStr(message.length()) + message;
+
+    // Przesłanie wiadomości do klienta
+    send (sck, finalMessage.c_str(), finalMessage.length(), 0);
 }
 
 
-void refreshChatList() {
-	
-	for (list<client>::iterator i = clients.begin(); i != clients.end(); ++i) {
 
-		string messageType = "2;";
-		string usersList = "";
-		for (list<client>::iterator it = clients.begin(); it != clients.end(); ++it) 	
-			if (i->chatRoom == it->chatRoom) {
-				usersList += ";" + it->name;
-			}
+/** Funkcja odpowiadająca za odświeżenie listy użytkowników w danym pokoju czatu
+ *
+ * Przesyła informację do każdego klienta z nową listą użytkowników znajdujących się w jego pokoju czatu
+ *
+ */
+void refreshRoomUsersList() {
 
-		sendMessageToClient(i->sck, "2", usersList);
-	}
+    // Przejrzenie całej tablicy użytkowników czatu
+    for (list<client>::iterator i = clients.begin(); i != clients.end(); ++i) {
+
+        string messageType = "2;";
+        string usersList = "";
+
+        for (list<client>::iterator it = clients.begin(); it != clients.end(); ++it)
+            if (i->chatRoom == it->chatRoom) {
+                usersList += ";" + it->name;
+            }
+
+        // Przesłanie do odpowiedniego klienta zaktualizowanej listy użytkowników
+        sendMessageToClient(i->sck, "2", usersList);
+    }
 }
 
-void changeChat(int sck, string chatName) {
 
-	for (list<client>::iterator it = clients.begin(); it != clients.end(); ++it) {
-	
-		if (it->sck == sck) {
-			it->chatRoom = chatName;
-			string welcomeMessage = ";<font color=\"DimGray \">Hi <font color=\"MidnightBlue \">" + it->name + 
-						"</font>! <br>Welcome to <font color=\"MidnightBlue \">" + chatName +"</font>!<br>" + 
-						"Respect other chat users and have fun!</font><br>";
-			
-			sendMessageToClient(sck, "1", welcomeMessage);
-			refreshChatList();
-			break;
-		}
-	}
+
+/** Funkcja zmiany pokoju czatu dla wybranego klienta
+ *
+ * @param sck Gniazdo klienta, który zmienia pokój czatu
+ * @param chatName Nazwa nowego pokoju czatu
+ *
+ */
+void changeChatRoom(int sck, string chatName) {
+
+    for (list<client>::iterator it = clients.begin(); it != clients.end(); ++it) {
+
+        if (it->sck == sck) {
+            it->chatRoom = chatName;
+            string welcomeMessage = ";<font color=\"DimGray \">Hi <font color=\"MidnightBlue \">" + it->name +
+                                    "</font>! <br>Welcome to <font color=\"MidnightBlue \">" + chatName +"</font>!<br>" +
+                                    "Respect other chat users and have fun!</font><br>";
+
+            sendMessageToClient(sck, "1", welcomeMessage);
+
+            // Odświeżenie listy użytkowników w każdym z pokoi
+            refreshRoomUsersList();
+            break;
+        }
+    }
 }
 
+
+
+/** Funkcja przygotowująca nowego klienta do korzystania z czatu
+ *
+ * @param sck Gniazdo nowego klienta
+ * @param name Nazwa nowego klienta
+ *
+ */
 void prepareClient(int sck, string name) {
-	
-	client newClient;
-	newClient.sck = sck;
-	newClient.name = name;
-	newClient.color = randomColor();
-	clients.push_back(newClient);
-	changeChat(sck, "Main Chat");
 
-	string chatRoomsList = "";
-	for (int i = 0; i < sizeof(chatRooms) / sizeof(chatRooms[0]); i++) {
-		chatRoomsList += ";" + chatRooms[i];
-	}
+    // Dodanie klienta do listy klientów
+    client newClient;
+    newClient.sck = sck;
+    newClient.name = name;
+    newClient.color = randomColor();
+    clients.push_back(newClient);
 
-	sendMessageToClient(sck, "3", chatRoomsList);
+    // Standardowo klient dodawany jest do pokoju głównego czatu
+    changeChatRoom(sck, "Main Chat");
+
+    string chatRoomsList = "";
+    for (int i = 0; i < sizeof(chatRooms) / sizeof(chatRooms[0]); i++) {
+            chatRoomsList += ";" + chatRooms[i];
+    }
+
+    // Przesłanie listy dostępnych czatów
+    sendMessageToClient(sck, "3", chatRoomsList);
 }
 
 
-
+/** Funkcja wywoływana gdy klient opuścił czat
+ *
+ * @param sck Gniazdo klienta
+ *
+ */
 void clientLeft(int sck) {
-	for (list<client>::iterator i = clients.begin(); i != clients.end();) {
-		if (i->sck == sck) 
-			i = clients.erase(i);
-		 else 
-			++i;
-	}
-	refreshChatList();
 
+    // Znalezienie i usunięcie wybranego klienta z listy klientów
+    for (list<client>::iterator i = clients.begin(); i != clients.end();) {
+        if (i->sck == sck)
+            i = clients.erase(i);
+         else
+            ++i;
+    }
+
+    // Odświeżenie listy użytkowników
+    refreshRoomUsersList();
+
+    // Zamknięcie gniazda
     close (sck);
+
     pthread_mutex_lock (&sock_mutex);
+
+    // Zwolnienie gniazda
     for (int i = 0; i < CON_LIMIT; i++)
         if (cln_sockets [i] == sck) {
             cln_sockets [i] = 0;
             break;
         }
+
     pthread_mutex_unlock (&sock_mutex);
 
-    printf ("Client thread ending for socket %d\n", sck);
+    printf ("Client left socket %d is now open\n", sck);
     pthread_exit (NULL);
-
 }
 
-void sendMessage(int sck, string clientName, string color, string message) {
 
-	bool showTime = true;
-	string finalMessage;
 
-	if (showTime) {
-		time_t rawtime;
-		time (&rawtime);
-		struct tm *time_info = localtime(&rawtime);
-		string minutes, hour;
+/** Funkcja przygotowująca wiadomość czatu do wysyłki
+ *
+ * @param sck Gniazdo klienta, który chce wysłać wiadomość
+ * @param clientName Nazwa klienta
+ * @param color Kolor klienta
+ * @param message Treść wiadomości
+ *
+ */
+void prepareChatMessage(int sck, string clientName, string color, string message) {
 
-		if (time_info->tm_min / 10 == 0) minutes = "0" + intToStr(time_info->tm_min);
-		else minutes = intToStr(time_info->tm_min); 
+    bool showTime = true;
+    string finalMessage;
 
-		if (time_info->tm_hour / 10 == 0) hour = "0" + intToStr(time_info->tm_hour);
-		else hour = intToStr(time_info->tm_hour);
-		string time = hour + ":" + minutes;
+    // Czy chcemy wyświetlać godzinę na czacie?
+    if (showTime) {
+        time_t rawtime;
+        time (&rawtime);
+        struct tm *time_info = localtime(&rawtime);
+        string minutes, hour;
 
-		finalMessage = ";<html><p><font color=\"Silver\">" + time + "</font> <b><font color=\"" + color + "\">" + clientName + "</b></font>: " + message + "</p></html>";
-	} else {
-		finalMessage = ";<html><p><b><font color=\"" + color + "\">" + clientName + "</b></font>: " + message + "</html>";
-	}
+        // Sprawdzenie czy otrzymana wartość minuty nie jest z przedziału <1;6> jeżeli jest to dopisanie "0" przed minutą
+        if (time_info->tm_min / 10 == 0) minutes = "0" + intToStr(time_info->tm_min);
+        else minutes = intToStr(time_info->tm_min);
 
-	sendMessageToClient(sck, "1", finalMessage);
+         // Sprawdzenie czy otrzymana wartość godziny nie jest z przedziału <1;6> jeżeli jest to dopisanie "0" przed godziną
+        if (time_info->tm_hour / 10 == 0) hour = "0" + intToStr(time_info->tm_hour);
+        else hour = intToStr(time_info->tm_hour);
+
+
+        string time = hour + ":" + minutes;
+        finalMessage = ";<html><p><font color=\"Silver\">" + time + "</font> <b><font color=\"" + color + "\">" + clientName + "</b></font>: " + message + "</p></html>";
+    } else
+        finalMessage = ";<html><p><b><font color=\"" + color + "\">" + clientName + "</b></font>: " + message + "</html>";
+
+    // Przesłanie wiadomości do klienta
+    sendMessageToClient(sck, "1", finalMessage);
 }
 
-void chatMessage(int sck, string message) {
-	for (list <client>::iterator it = clients.begin(); it != clients.end(); ++it) {
-		if (sck == it->sck) {
-			string clientChatRoom = it->chatRoom;
-			string clientName = it->name;
-			string clientColor = it->color;
 
-			for (list <client>::iterator rest = clients.begin(); rest != clients.end(); ++rest) {
-				if (rest->chatRoom == clientChatRoom) {
-					sendMessage(rest->sck, clientName, clientColor, message);
-					cout << rest->sck << " " << rest->color << endl;
-				}
-			}
-			break;
-		}
-	}
+
+/** Funkcja wywoływana gdy otrzymano przyszła nowa wiadomość na czacie
+ *
+ * @param sck Gniazdo klienta, który przesłał wiadomość czatu
+ * @param message Wiadomość którą przesyła klient
+ *
+ */
+void chatMessageReceived(int sck, string message) {
+
+    // Znalezienie klienta, który przesyła wiadomość
+    for (list <client>::iterator it = clients.begin(); it != clients.end(); ++it) {
+        if (sck == it->sck) {
+            string clientChatRoom = it->chatRoom;
+            string clientName = it->name;
+            string clientColor = it->color;
+
+            // Przesłanie do wszystkich innych klientów w tym pokoju tej wiadomości
+            for (list <client>::iterator rest = clients.begin(); rest != clients.end(); ++rest) {
+                if (rest->chatRoom == clientChatRoom) {
+                    prepareChatMessage(rest->sck, clientName, clientColor, message);
+                    cout << rest->sck << " " << rest->color << endl;
+                }
+            }
+
+            break;
+        }
+    }
 }
 
+
+
+/** Funkcja obsługująca otrzymaną wiadomość
+ *
+ * @param sck Gniazdo klienta, który przesłał wiadomość do serwera
+ * @param buffer Bufor z przesłanymi danymi
+ *
+ */
 void handleReceivedMessage(int sck, char *buffer) {
-	
-	string message(buffer);
-	string delimiter = ";";
-	string token;
-	size_t pos = 0;
 
-	while ((pos = message.find(delimiter)) != string::npos) {
-		token = message.substr(0, pos);
-		message.erase(0, pos + delimiter.length());
-		if (token == "0") {
-			prepareClient(sck, message);
-		} else if (token == "1") {
-			chatMessage(sck, message);
-		} else if (token == "2") {
+    string message(buffer);
+    string delimiter = ";";
+    string token;
+    size_t pos = 0;
 
-		} else if (token == "3") {
-			changeChat(sck, message);
-		}
-	}
+    // Sprawdzenie jaki typ wiadomości został wysłany
+    while ((pos = message.find(delimiter)) != string::npos) {
+        token = message.substr(0, pos);
+        message.erase(0, pos + delimiter.length());
 
-	fflush(stdout);
+        if (token == "0")
+            prepareClient(sck, message);
+        else if (token == "1")
+            chatMessageReceived(sck, message);
+        else if (token == "2")
+            ; // TODO możliwe usuwanie użytkownika z czatu
+        else if (token == "3")
+            changeChatRoom(sck, message);
+    }
 }
 
-void* client_loop (void* arg)
-{
+
+/** Główna pętla klienta na serwerze, w której nasłuchuje on wiadomości od siebie (klienta)
+ *
+ * @param arg Przesyłane gniazdo klienta, który podłączył się do czatu
+ * @return Wskaźnik na metodę
+ *
+ */
+void* client_loop (void* arg) {
+
     char buffer [1024];
     int sck = *((int*) arg);
-    int i;
     int rcvd;
 
-    printf ("New client thread started for socket %d\n", sck);
+    printf ("New client has connected. Socket %d is now taken!\n", sck);
 
-  	while( (rcvd = recv(sck , buffer, 1024 , 0)) > 0 ){	
-        	handleReceivedMessage(sck, buffer);
-		cout << buffer << endl;
-		memset (&buffer, 0, 1024);
-   	}
-	
-	clientLeft(sck);
+    // Oczekiwanie na wiadomości od klienta przychodzące pod dany socket
+    while( (rcvd = recv(sck , buffer, 1024 , 0)) > 0 ) {
+
+        // Przekazanie otrzymanej wiadomości do funkcji, która sprawdzi jakiego typu to wiadomość
+        handleReceivedMessage(sck, buffer);
+        memset (&buffer, 0, 1024);
+    }
+
+    // Jeżeli klient opuści serwer wywoływana jest funkcja czyszcząca dane związane z klientem
+    clientLeft(sck);
 }
 
+
+
+/** Główna pętla serwera, w której nasłuchuje on czy nie próbują podłączyć się nowi klienci
+ * @param arg
+ * @return Wskaźnik na metodę
+ *
+ */
 void* main_loop (void* arg) {
+
+    // Deklaracja niezbędnych zmiennych i struktur
     int srv_socket, tmp_socket;
     socklen_t cln_addr_size;
     struct sockaddr_in srv_addr, cln_addr;
 
+    // Szczegółowe dane dotyczące typu połączenia
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_addr.s_addr = INADDR_ANY;
     srv_addr.sin_port = htons (PORT);
 
+    // Wyczyszczenie tablicy gniazd klientów
     bzero (cln_sockets, CON_LIMIT * sizeof (int));
 
+    // Próba uzyskania gniazda serwera
     if ((srv_socket = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
         perror ("Main socket creation error:");
         exit (EXIT_FAILURE);
     }
 
+    // Próba przypisania gniazda do struktury adresowej
     if (bind (srv_socket, (struct sockaddr*) &srv_addr, sizeof srv_addr) == -1) {
         perror ("Main socket bind error:");
         exit (EXIT_FAILURE);
     }
 
+    // Próba nasłuchiwania
     if (listen (srv_socket, 16) == -1) {
         perror ("Main socket listen error:");
         exit (EXIT_FAILURE);
     }
 
-    printf ("Main socket started\n");
+    printf ("Server is ready to go!\n");
 
+    // Nasłuchiwanie czy nowi klienci nie chcą się połączyć z serwerem
     while (1) {
         cln_addr_size = sizeof cln_addr;
+
+        // Jeżeli przyjdzie zapytanie o akceptacje (protokół TCP) to wiemy, że nowy klient próbuje nawiązać z serwerem połączenie
         if ((tmp_socket = accept (srv_socket, (struct sockaddr*) &cln_addr, &cln_addr_size)) == -1) {
             perror ("Main socket accept error:");
             continue;
         }
 
+        // Podnosimy semafor (aby wyeliminować niezgodności związane z wielowątkową pracą serwera)
         pthread_mutex_lock (&sock_mutex);
+
         int i;
+
+        // Dopisanie nowego klienta do listy gniazd (o ile jest miejsce dla klienta)
         for (i = 0; i < CON_LIMIT; i++)
             if (cln_sockets [i] == 0) {
                 cln_sockets [i] = tmp_socket;
                 break;
             }
+
+        // Opuszczenie semafora
         pthread_mutex_unlock (&sock_mutex);
 
+        // Sprawdzenie czy nie osiągneliśmy maksymalnej liczby podłączonych klientóW
         if (i == CON_LIMIT) {
             printf ("Too many connections\n");
             close (tmp_socket);
             continue;
         }
 
+        // Utworzenie nowego wątku dla obsługi klienta, aby mógł nasłuchiwać przychodzących wiadomości od klienta
         if (pthread_create (&client_threads [i], NULL, client_loop, &cln_sockets[i]) != 0) {
             printf ("Error creating new client thread\n");
             continue;
@@ -280,18 +423,29 @@ void* main_loop (void* arg) {
     }
 }
 
+
+
+/** Funkcja startowa programu
+ *
+ * @param argc Liczba argumentów
+ * @param argv Lista argumentów
+ * @return Wartość wykonania programu
+ *
+ */
 int main (int argc, char** argv) {
 
-	srand(time(NULL));
-    if (pthread_create (&main_thread, NULL, main_loop, NULL) != 0) {
+        srand(time(NULL));
+
+        // Utworzenie nowego głównego wątku nasłuchującego czy nowi klienci chcą się przyłączyć do serwera
+        if (pthread_create (&main_thread, NULL, main_loop, NULL) != 0) {
         printf ("Thread create error\n");
         exit (EXIT_FAILURE);
-    }
+        }
 
-    printf ("Server has started\n");
-    printf ("Press <ENTER> to shutdown server\n");
-    getc (stdin);
+        printf ("Server has started\n");
+        printf ("Press <ENTER> to shutdown server\n");
+        getc (stdin);
 
-    return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
 
